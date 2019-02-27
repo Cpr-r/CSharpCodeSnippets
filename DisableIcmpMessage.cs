17b8f00df1276ff18106ae2c542260f464c56e22
Curtis <47208537+Cpr-r@users.noreply.github.com>
2019-02-27 20:23:05 +1000

    Update DisableIcmpMessage.cs

--- DisableIcmpMessage.cs
+++ DisableIcmpMessage.cs
@@ -1,7 +1,8 @@
 /* 
 I am in reference to the error that occurs when the client does not handle disconnect logic properly. 
 That is, the UDP server crashes because the UDP client does not exit gracefully or crashes, thereby forcing a 'hard-disconnect'.  
-If you suspect that you have this error, a simple trick to reproduce it is to close--all at once--dozens of clients who are sending packets to your server.
+If you suspect that you have this error, a simple trick to reproduce it is to close--all at once--
+dozens of clients who are sending packets to your server.
 Note: this bug will only occur if you are using asynchronous methods for transmitting packets.
 
 The explanation for why this issue occurs is simple: The UDP protocol is also listening to ICMP messages, 
