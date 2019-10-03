4e63f2c39ce6441e686e4c2c05e3d4bfe7d28951
doubletruth <47208537+doubletruth@users.noreply.github.com>
2019-10-03 20:52:27 +1000

    Upgraded the asynchronous handling of sending and receiving messages by virtue of local functions (instead of delegate capturing). Updated code clarity and syntax as well.

--- Networking/AsyncUdpClientServer.cs
+++ Networking/AsyncUdpClientServer.cs
@@ -5,83 +5,86 @@ using System.Net.Sockets;
 
 namespace MyProgram.Networking
 {
-    // It's typically wise to give this class its own file space.
-    internal sealed class StateObject
+    public sealed class StateObject
     {
-        internal byte[] Buffer = new byte[UdpSocket.BUFFER_SIZE];
-        internal Socket Socket = null;
+        public byte[] Buffer = new byte[UdpSocket.Buffer_Size];
+        public Socket Socket;
     }
 
-    internal sealed class UdpSocket
+    public sealed class UdpSocket
     {  
         /// <summary>
-        /// The size of the UDP buffer.
+        /// The size of the buffer object (the object where packet data is stored).
         /// </summary>
-        internal const int BUFFER_SIZE = 8 * 1_024;
+        public const int Buffer_Size = 8 * 1_024;
 
         /// <summary>
         /// The enum code for disabling ICMP messages on disconnected UDP sockets.
         /// </summary>
-        private const int SOCKET_IO_UDP_CONNRESET = -1_744_830_452;
+        /// <remarks>See DisableIcmpMessage.cs.</remarks>
+        private const int socket_io_udp_conn_reset = -1_744_830_452;
 
         /// <summary>
-        /// The UDP socket, in effect.
+        /// The wrapper class for the UDP socket and buffer.
         /// </summary>
         private readonly StateObject stateObject = new StateObject();
 
         /// <summary>
-        /// The end point to 'listen' on. That is, this socket can receive messages from all ports.
+        /// The end point to 'listen' on; this socket can receive messages from all ports.
         /// </summary>
         private EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
 
         /// <summary>
         /// For the asynchronous socket receive/send implementation.
         /// </summary>
-        private AsyncCallback receiveCallback = null;
+        private AsyncCallback receiveCallback;
 
         /// <summary>
-        /// Initialise and configure the underlying socket. 
+        /// Initialises and configures the state object's socket. 
         /// </summary>
-        internal void BeginTransmission(int port)
+        public void BeginTransmission(int port)
         {
             this.stateObject.Socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             this.stateObject.Socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
-            this.stateObject.Socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
-            // See DisableIcmpMessage.cs
-            this.stateObject.Socket.IOControl((IOControlCode)SOCKET_IO_UDP_CONNRESET, new byte[] { 0, 0, 0, 0 }, null);
-
-            // Begin the actual transmission.
+            this.stateObject.Socket.Bind(new IPEndPoint(IPAddress.Parse("localhost"), port));
+            this.stateObject.Socket.IOControl((IOControlCode)socket_io_udp_conn_reset, new byte[] { 0, 0, 0, 0 }, null);
             this.Receive();
         }
 
         /// <summary>
-        /// Sends a message to the destination end point.
+        /// An overload for Send that accepts string (instead of byte[]) and automatically converts the string to bytes.
         /// </summary>
-        internal void Send(byte[] message, EndPoint destination)
+        /// <remarks>Change ASCII string encoding if necessary.</remarks>
+        public void Send(string message, EndPoint destination) => this.Send(Encoding.ASCII.GetBytes(message), destination);
+
+        /// <summary>
+        /// Sends passed message to passed destination end point.
+        /// </summary>
+        public void Send(byte[] message, EndPoint destination)
         {
-            this.stateObject.Socket.BeginSendTo(message, 0, message.Length, SocketFlags.None, destination, (asyncResult) =>
+            void SendCallback(IAsyncResult asyncResult)
             {
                 int bytes = this.stateObject.Socket.EndSendTo(asyncResult);
-            },
-            this.stateObject);
+            }
+            this.stateObject.Socket.BeginSendTo(message, 0, message.Length, SocketFlags.None, destination, SendCallback, this.stateObject);
         }
 
         /// <summary>
-        /// Receives a message from all ports on this local machine.
+        /// Receives messages from all ports on this local machine.
         /// </summary>
         private void Receive()
         {
-            this.stateObject.Socket.BeginReceiveFrom(this.stateObject.Buffer, 0, BUFFER_SIZE, SocketFlags.None, ref this.endPoint, this.receiveCallback = (asyncResult) =>
+            void ReceiveCallback(IAsyncResult asyncResult) 
             {
                 var localStateObject = (StateObject)asyncResult.AsyncState;
 
                 int bytes = this.stateObject.Socket.EndReceiveFrom(asyncResult, ref this.endPoint);
-                byte[] message = this.stateObject.Buffer;
+                byte[] message = this.stateObject.Buffer; // Remember, the buffer size is 8 * 1_024. Therefore, you must trim the excess bytes when passing it to external state.
 
                 // Begin the async receive loop.
-                this.stateObject.Socket.BeginReceiveFrom(localStateObject.Buffer, 0, BUFFER_SIZE, SocketFlags.None, ref this.endPoint, this.receiveCallback, localStateObject);
-            },
-            this.stateObject);
+                this.stateObject.Socket.BeginReceiveFrom(localStateObject.Buffer, 0, Buffer_Size, SocketFlags.None, ref this.endPoint, this.receiveCallback, localStateObject);
+            }
+            this.stateObject.Socket.BeginReceiveFrom(this.stateObject.Buffer, 0, Buffer_Size, SocketFlags.None, ref this.endPoint, this.receiveCallback = ReceiveCallback, this.stateObject);
         }
     }
 }
